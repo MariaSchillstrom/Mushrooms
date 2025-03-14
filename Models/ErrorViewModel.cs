64caba2d809d5b9f4ee98343d5d38c7783f13a96
maria schillstrom <mariacschillstrom@gmail.com>
2025-03-14 13:09:25 +0100

    Initial commit: Created ASP.NET Core MVC application

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace WeLoveMushrooms.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
