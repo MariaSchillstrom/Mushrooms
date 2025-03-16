ec6d858eb47b8566f222e160be68299c9b78449c
maria schillstrom <mariacschillstrom@gmail.com>
2025-03-16 20:37:37 +0100

    Set ups for the application

---
+++ Controllers/NewsletterController.cs
@@ -0,0 +1,30 @@
+using CloudSoft.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CloudSoft.Controllers;
+
+public class NewsletterController : Controller
+{
+
+    [HttpGet]
+    public IActionResult Subscribe()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult Subscribe(Subscriber subscriber)
+    {
+        // Add subscription logic here
+        // ...
+
+        // Write to the console
+        Console.WriteLine($"New subscription - Name: {subscriber.Name} Email: {subscriber.Email}");
+        
+        // Send a message to the user
+        ViewBag.Message = $"Thank you for subscribing, {subscriber.Name}!";
+
+        // Return the view
+        return View();
+    }
+}
